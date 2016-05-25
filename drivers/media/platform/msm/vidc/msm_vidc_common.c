4a9a0f9fbfcfb8e915b7b3a672ebbfb8f30575dc
Praneeth Paladugu <ppaladug@codeaurora.org>
2016-05-24 22:38:40 -0700

    msm: vidc: Add support for color space information
    When clients specify color space information, venus HW
    should embed it in the bit stream. This helps to
    regenerate original colors when decoded by decoder.
    When decoder detects the color space VUI, Venus fills
    the corresponding extradata.
    
    CRs-Fixed: 1026204
    Change-Id: Ie9523558c23a8edc7a7ee058937658dd87ef5b16
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -81,6 +81,8 @@ const char *const mpeg_video_vidc_extradata[] = {
 	"Extradata output crop",
 	"Extradata display colour SEI",
 	"Extradata light level SEI",
+	"Extradata display VUI",
+	"Extradata vpx color space",
 };
 
 struct getprop_buf {
@@ -4118,6 +4120,12 @@ enum hal_extradata_id msm_comm_get_hal_extradata_index(
 		break;
 	case V4L2_MPEG_VIDC_EXTRADATA_CONTENT_LIGHT_LEVEL_SEI:
 		ret = HAL_EXTRADATA_CONTENT_LIGHT_LEVEL_SEI;
+		break;
+	case V4L2_MPEG_VIDC_EXTRADATA_VUI_DISPLAY:
+		ret = HAL_EXTRADATA_VUI_DISPLAY_INFO;
+		break;
+	case V4L2_MPEG_VIDC_EXTRADATA_VPX_COLORSPACE:
+		ret = HAL_EXTRADATA_VPX_COLORSPACE;
 		break;
 	default:
 		dprintk(VIDC_WARN, "Extradata not found: %d\n", index);
