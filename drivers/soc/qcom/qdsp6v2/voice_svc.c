8d7433c799d829322e0c2c88b7b211bc2880af0c
smanag <smanag@codeaurora.org>
2017-11-14 14:57:57 +0530

    drivers: soc: Synchronize apr callback and voice svc release
    Issue is seen when apr callback is received while voice_svc_release
    is in process of freeing the driver private data.
    Avoid invalid access of private data pointer by putting
    the callback and release functions in the same locked context.
    
    Change-Id: I93af13cab0a3c7e653a9bc9fa7f4f86bfa0502df
    Signed-off-by: smanag <smanag@codeaurora.org>

--- drivers/soc/qcom/qdsp6v2/voice_svc.c
+++ drivers/soc/qcom/qdsp6v2/voice_svc.c
@@ -68,7 +68,7 @@ static void *dummy_q6_mvm;
 static void *dummy_q6_cvs;
 dev_t device_num;
 
-static spinlock_t voicesvc_lock;
+spinlock_t voicesvc_lock;
 static bool is_released;
 static int voice_svc_dummy_reg(void);
 static int voice_svc_dummy_dereg(void);
