7c1819f5e203b4a23e17ee7d81d3820c51461396
Marc Zyngier <marc.zyngier@arm.com>
2018-01-11 14:28:38 +0000

    arm: Invalidate BTB on prefetch abort outside of user mapping on Cortex A8, A9, A12 and A17.
    In order to prevent aliasing attacks on the branch predictor,
    invalidate the BTB on CPUs that are known to be affected when taking
    a prefetch abort on a address that is outside of a user task limit.
    
    Change-Id: I5d35c4f7b686149187603b110e9fed8f61f9fc3f
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Git-commit: b606e231365d6550302c2375cb20805f9aada11d
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/arm64/linux
    [neeraju@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>
    Signed-off-by: Rajshekar Eashwarappa <reashw@codeaurora.org>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -421,17 +421,11 @@ do_pabt_page_fault(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 		case ARM_CPU_PART_CORTEX_A8:
 			write_sysreg(0, BPIALL);
 			break;
-
-		case ARM_CPU_PART_CORTEX_A57:
-		case ARM_CPU_PART_CORTEX_A72:
-			write_sysreg(0, ICIALLU);
-			break;
 		}
 	}
 
 	return do_page_fault(addr, fsr, regs);
 }
-
 #else					/* CONFIG_MMU */
 static int
 do_page_fault(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
