72ed05dca5b30c95053187ba024d40d89a7e3e91
vm03 <vasy@vasy.ru>
2018-02-01 16:29:29 +0300

    make mrproper

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Nov 2 17:07:11 KST 2016"
-#define LINUX_COMPILE_BY "dao.zheng"
-#define LINUX_COMPILE_HOST "R0709-09"
-#define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (GCC) "
