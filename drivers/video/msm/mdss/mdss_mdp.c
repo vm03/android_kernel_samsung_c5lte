e28a93f58662637c9faf69ecacca695f7c46f472
raghavendra ambadas <rambad@codeaurora.org>
2018-06-04 10:56:52 +0530

    msm: mdss: Fix double free during probe fail
    Memory allocated with "devm_kzalloc" is automatically released
    by the kernel if the "probe" function fails with an error code.
    Therefore, using "kfree" is unsafe because it can lead to the Double-Free.
    
    Change-Id: Ic9285ebbd7d246e275a93cde4d03656d99d5ea3d
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -1,7 +1,7 @@
 /*
  * MDSS MDP Interface (used by framebuffer core)
  *
- * Copyright (c) 2007-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2007-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 2007 Google Incorporated
  *
  * This software is licensed under the terms of the GNU General Public
